5a7e4d6ad3cfb5b8d1519747029d6f19bc013d9c
Aidan Cunniffe <acunniffe@gmail.com>
2019-03-05 17:37:16 -0500

    Reorganized Gem

--- spec/optic/middleware_spec.rb
+++ spec/optic/middleware_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Optic::Middleware do
   it "has a version number" do
     expect(Optic::Middleware::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
