f1695f3a71e83030c88dbeb5ffe6f449f5831e25
Aidan Cunniffe <acunniffe@gmail.com>
2019-03-05 17:29:06 -0500

    Initial Commit

---
+++ lib/optic/middleware.rb
@@ -0,0 +1,8 @@
+require "optic/middleware/version"
+
+module Optic
+  module Middleware
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/optic/middleware/version.rb
@@ -0,0 +1,5 @@
+module Optic
+  module Middleware
+    VERSION = "0.1.0"
+  end
+end
